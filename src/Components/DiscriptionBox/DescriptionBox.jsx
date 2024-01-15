84238d45947320aee28d1090e7115abd974065b7
SahanDev00 <dumidus86@gmail.com>
2024-01-15 20:19:58 +0530

    Modified

---
+++ src/Components/DiscriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-nav">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem in impedit aut laboriosam velit, officiis voluptate. Et mollitia obcaecati quibusdam. Dolorum, autem quod labore repellendus ex animi tempora magni recusandae!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam repellat tenetur odio tempore nostrum molestiae fuga cupiditate adipisci, unde delectus ipsum. Minima labore quod iste earum ad iusto nihil perferendis.</p>
+            </div>
+        </div>
+    )
+}
